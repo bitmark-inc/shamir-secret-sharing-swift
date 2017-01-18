7fe3adeadf89b9a02450c462d2c00fed85f50e75
Anh Nguyen <anhnguyen@bitmark.com>
2017-01-18 11:15:00 +0700

    Using submodule for external lib

--- ShamirSecretSharing/Dependencies/strtok.h
+++
@@ -1,9 +0,0 @@
-/* This file is only included since MINGW doesn't have strtok_r, so I can't
-   compile for Windows without this */
-
-#include <string.h>
-
-char* strtok_rr(
-    char *str, 
-    const char *delim, 
-    char **nextp);
